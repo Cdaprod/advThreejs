4edfac5c6e77e030e972362a9b9cd81735778e71
David <104325852+Cdaprod@users.noreply.github.com>
2023-04-04 17:39:56 -0400

    .

---
+++ ThreeComponent.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+
+export function initThreeComponent(container) {
+  // Create a scene
+  const scene = new THREE.Scene();
+
+  // Create a camera
+  const camera = new THREE.PerspectiveCamera(75, container.offsetWidth / container.offsetHeight, 0.1, 1000);
+  camera.position.z = 5;
+
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(container.offsetWidth, container.offsetHeight);
+  container.appendChild(renderer.domElement);
+
+  // Create a geometry
+  const geometry = new THREE.BoxGeometry();
+
+  // Create a material
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+  // Create a mesh
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  // Animation loop
+  function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotate the cube
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    // Render the scene
+    renderer.render(scene, camera);
+  }
+
+  animate();
+}
